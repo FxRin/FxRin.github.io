74eace50d6e2721e504c1161e304c67ed3fb44ea
Niramon C. <niramon4671@gmail.com>
2024-08-08 02:43:04 +0700

    test T

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrizeController;
 use App\Http\Controllers\ParticipantController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/', [PrizeController::class, 'index'])->name('prizes.index');
 Route::post('/draw', [PrizeController::class, 'draw'])->name('prizes.draw');
